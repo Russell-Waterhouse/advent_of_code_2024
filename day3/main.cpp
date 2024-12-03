e2f8fd8a8c9f7bd2600a34ae3c22ea352dd5651a
Russell-Waterhouse <Russell.L.Waterhouse@gmail.com>
2024-12-03 13:27:28 -0700

    day 3 part 2

--- day3/main.cpp
+++ day3/main.cpp
@@ -26,6 +26,52 @@ std::vector<std::string> split(std::string input, std::string delimiter) {
     return tokens;
 }
 
+std::vector<std::string> remove_conditional_commands(std::vector<std::string> input) {
+    // 1. Remove everything from don't() to do()
+    // I know this doesn't work for anything that wraps around lines,
+    // but maybe I won't hit that case.
+    std::regex r {"don't\\(\\).*?do\\(\\)"};
+    std::smatch m;
+    std::vector<std::string> new_input {};
+    size_t pos {0};
+    for (size_t i {0}; i < input.size(); i++) {
+        std::regex_search(input[i], m, r);
+        size_t num_matches = m.size();
+
+        while (num_matches > 0) {
+            std::string v = m[0];
+            if (v == "") {
+                break;
+            }
+            std::cout << "Finding : " << v << "\n";
+            pos = input[i].find(v);
+            if (pos == std::string::npos) {
+                throw new std::exception();
+            }
+            std::cout << "Found at position: " << pos << "\n";
+            input[i].erase(pos, v.length());
+            std::regex_search(input[i], m, r);
+            size_t num_matches = m.size();
+        }
+        std::cout << "new input line is: " << input[i] << std::endl;
+        new_input.push_back(input[i]);
+    }
+
+    // this is sloppy because I am so close
+
+
+    // remove the first don't to the end of the line
+    std::regex new_r {"don't\\(\\).*$"};
+    // also, I put everything on one line to make this easier
+    std::regex_search(new_input[0], m, new_r);
+    std::string s { m[0] };
+    pos = new_input[0].find(s);
+
+    new_input[0].erase(pos, s.length());
+
+    return new_input;
+}
+
 std::vector<std::string> extract_multiplication_commands(std::string input) {
     auto multiplication_commands = std::vector<std::string> {};
     std::regex r("mul\\(\\d+,\\d+\\)");
@@ -92,6 +138,15 @@ int main(int argc, char *argv[]) {
     std::cout << "The answer to part 1 is: " << result << std::endl;
     // TODO: use regex to just delete everything from "don't().*do()
     // Then call the same thing that I did in part 1
+    std::vector<std::string> part_2_input = remove_conditional_commands(rawInput);
+    multiplication_commands = {};
+    for (auto l: part_2_input){
+        std::cout << "Processing new line: " << l << std::endl;
+        std::vector<std::string> new_multiplication_commands = extract_multiplication_commands(l);
+        multiplication_commands.insert(multiplication_commands.end(), new_multiplication_commands.begin(), new_multiplication_commands.end());
+    }
+    result = calculate_multiplications(multiplication_commands);
+    std::cout << "The answer to part 2 is: " << result << std::endl;
 
     return 0;
 }
