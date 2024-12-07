6e01c959bc4a4e1c93c9a525594da4e802689732
Russell-Waterhouse <Russell.L.Waterhouse@gmail.com>
2024-12-07 12:55:35 -0700

    broken day 7 part 2

--- day7/main.cpp
+++ day7/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <optional>
 #include <vector>
 #include <string>
 #include <cmath>
@@ -34,6 +35,65 @@ std::vector<std::string> split(std::string input, std::string delimiter) {
   // std::cout << "Returning" << std::endl;
   // print_vector(tokens);
   return tokens;
+}
+
+enum class Operator {
+  kAdd,
+  kMultiply,
+  kConcat
+};
+
+
+std::optional<Operator> nextOperator(Operator& op) {
+  switch(op) {
+    case Operator::kAdd:
+      return Operator::kMultiply;
+    case Operator::kMultiply:
+      return Operator::kConcat;
+    case Operator::kConcat:
+      return {};
+  }
+  return {};
+}
+
+// 0 0 0
+// 0 0 1
+// 0 0 2
+// 0 1 0
+// 0 1 1
+// 0 1 2
+// 0 2 0
+// 0 2 1
+// 0 2 2
+// 1 0 0
+// 1 0 0
+// 1 0 1
+// 1 0 2
+// 1 1 0
+// 1 1 1
+// 1 1 2
+// 1 2 0
+// 1 2 1
+// 1 2 2
+// 2 0 0
+
+std::optional <std::vector<Operator>> nextIteration(std::vector<Operator> operators) {
+  for (size_t i {operators.size()}; i <= 100; i--) {
+    std::optional<Operator> next_op = nextOperator(operators.at(i));
+    if (next_op.has_value()) {
+      operators.at(i) = next_op.value();
+      if ( i < operators.size() - 2) {
+        // go back through and reset all less significant bits to zero
+        for (size_t j {i + 1}; j < operators.size(); j++) {
+          operators.at(j) = Operator::kAdd;
+        }
+      }
+
+      return operators;
+    }
+  }
+
+  return {};
 }
 
 class Equation{
@@ -75,6 +135,59 @@ class Equation{
       }
       return 0;
     }
+
+    long int calculateEquation(std::vector<Operator>& operators) {
+      if (operators.size() == 0) {
+        std::cout << "Operators size is 0 :(" << std::endl;
+        // throw new std::exception;
+      }
+      long int calculation {right.at(0)};
+      for(size_t i {1}; i < right.size(); i++) {
+        switch (operators.at(i - 1)) {
+          case Operator::kAdd:
+            calculation += right.at(i);
+            break;
+          case Operator::kMultiply:
+            calculation *= right.at(i);
+            break;
+          case Operator::kConcat:
+            std::string s {std::to_string(calculation)};
+            s.append(std::to_string(right.at(i)));
+            calculation = std::stoi(s);
+            break;
+        }
+      }
+      if (left == calculation) {
+        return left;
+      }
+      return 0;
+    }
+
+    long int validEquationSum2() {
+      std::vector<Operator> ops {};
+      std::optional<std::vector<Operator>> operators {ops};
+      for (size_t i {0}; i < right.size() - 1; i++) {
+        operators.value().push_back(Operator::kAdd);
+      }
+      size_t i {0};
+      // if (!operators.has_value()) {
+        // std::cout << "Operators has no value :(" << std::endl;
+        // throw new std::exception;
+      // }
+      if (operators.value().size() < 1) {
+        throw new std::exception;
+      }
+      do {
+        i++;
+        std::cout << "Iteration " << i << std::endl;
+        long int result = calculateEquation(operators.value());
+        if (result != 0) {
+          return result;
+        }
+        operators = nextIteration(operators.value());
+      } while (operators.has_value());
+      return 0;
+    }
 };
 
 int main(int argc, char *argv[]) {
@@ -108,7 +221,11 @@ int main(int argc, char *argv[]) {
 
   std::cout << "The answer to part 1 is: " << validEquationSum << std::endl;
 
+  long int part2EquationSum {0};
+  for (auto equation: equations) {
+    part2EquationSum += equation.validEquationSum2();
+  }
 
-
+  std::cout << "The answer to part 2 is: " << part2EquationSum << std::endl;
   return 0;
 }
