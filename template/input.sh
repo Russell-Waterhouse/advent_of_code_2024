b6c7890f2ad71fc4bb02af6249e8c76d8620525d
Russell-Waterhouse <Russell.L.Waterhouse@gmail.com>
2024-12-09 14:57:13 -0700

    update template folder

--- template/input.sh
+++ template/input.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-# CPPLIBFLAGS = -c $(CPPFLAGS)
+echo "Building..."
+./build.sh
 
-g++ -g -o main main.cpp -std=c++23 -Wextra -Wall -Weffc++ -Werror -pedantic -pedantic-errors -Wconversion -Wsign-conversion -Wold-style-cast -Woverloaded-virtual -Wfloat-equal -Wwrite-strings -Wpointer-arith -Wcast-qual -Wcast-align -Wunreachable-code -Winit-self -Wno-variadic-macros -Wno-parentheses -Wno-unused-function -Wno-unused-parameter -Wno-unused-variable -Wno-unused-value -Wno-unused-but-set-variable -Wno-unused-local-typedefs
-
+echo "Running..."
 ./main input.txt
 
--- template/main.cpp
+++ template/main.cpp
@@ -1,52 +1,29 @@
 #include <cstdlib>
 #include <iostream>
-#include <fstream>
 #include <vector>
-#include <string>
+#include <cmath>
+#include "aoc_helper.hpp"
+
 
 template <typename T>
 void print_vector(std::vector<T>& vec) {
   for (auto& v : vec) {
-    std::cout << v << " ";
+    std::cout << v << " \n";
   }
+  std::cout << std::endl;
 }
-
-std::vector<std::string> split(std::string input, std::string delimiter) {
-  std::vector<std::string> tokens {};
-  size_t pos {0};
-  int i {0};
-  do {
-    pos = input.find(delimiter);
-    if (pos == std::string::npos) {
-      break;
-    }
-    std::string token = input.substr(0, pos);
-    std::cout << "Token is " << token << std::endl;
-    tokens.push_back(token);
-    input.erase(0, pos + delimiter.size());
-    i++;
-  } while (i < 100);
-  tokens.push_back(input);
-  return tokens;
-}
-
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     std::cout << "No argument given, exiting" << std::endl;
     return 0;
   }
-  std::string input_file_name = {argv[1]};
-  std::ifstream inputFile(input_file_name);
-  std::vector<std::string> rawInput {};
-  std::string line {};
-  while(std::getline(inputFile, line)) {
-    if(line == "") {
-      continue;
-    }
-    rawInput.push_back(line);
-  }
-  inputFile.close();
-
+  std::vector<std::string> rawInput = parseInput(argv[1]);
+  
+  int part_1 {0};
+  std::cout << "The answer to part 1 is: " << part_1 << std::endl;
+  
+  int part_2 {0};
+  std::cout << "The answer to part 2 is: " << part_2 << std::endl;
   return 0;
 }
